dabe0183b2beee6b70d6f829ae9608e70b9736af
Adam Sawicki <adamsawicki89@gmail.com>
2021-06-10 18:52:41 +0200

    Add explanation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <ul>
+        <li>Open developer console</li>
+        <li>Press Open Popover</li>
+        <li>Close popover by clicking outside</li>
+        <li>Keep clicking outside</li>
+        <li>Result: onClose event is fired</li>
+      </ul>
       <Popover
         trigger={<Button>Open Popover</Button>}
         onClose={() => console.log("on close")}
